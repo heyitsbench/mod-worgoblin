8c6748a7bab58683b40df173fc9524d511042a3e
benjymansy123 <benjymansy123@gmail.com>
2022-09-16 01:51:04 -0400

    Add workflow-specific SQL files.
    Part of #36.

---
+++ data/sql/db-world/workflow/creaturedisplayinfo_dbc.sql
@@ -0,0 +1,8 @@
+INSERT INTO `creaturedisplayinfo_dbc` (`ID`, `ModelID`, `SoundID`, `ExtendedDisplayInfoID`, `CreatureModelScale`, `CreatureModelAlpha`, `TextureVariation_1`, `TextureVariation_2`, `TextureVariation_3`, `PortraitTextureName`, `BloodLevel`, `BloodID`, `NPCSoundID`, `ParticleColorID`, `CreatureGeosetData`, `ObjectEffectPackageID`) VALUES
+(29422, 3141, 0, 0, 1, 255, NULL, NULL, NULL, NULL, 3, 0, 0, 0, 0, 0),
+(29423, 3142, 0, 0, 1, 255, NULL, NULL, NULL, NULL, 3, 0, 0, 0, 0, 0),
+(32385, 3194, 0, 0, 0.75, 255, 'Hobgoblin1Red', 'Hobgoblin2', NULL, NULL, 0, 0, 0, 0, 0, 0),
+(35249, 3624, 0, 0, 1, 255, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0),
+(35250, 3623, 0, 0, 1, 255, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0),
+(39095, 65, 0, 0, 1, 255, 'HorseSkinBlack', NULL, NULL, NULL, 1, 0, 0, 0, 0, 0),
+(39096, 65, 0, 0, 1, 255, 'HorseSkinBrown', NULL, NULL, NULL, 1, 0, 0, 0, 0, 0);
