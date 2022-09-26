fde2cd7d0155f8eb835449acc80f46b8d15186d0
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-09-26 13:56:01 -0400

    Change structure of SQL to match recent AC changes.

--- data/sql/db-world/workflow/skilllineability_dbc.sql
+++ data/sql/db-world/workflow/skilllineability_dbc.sql
@@ -1,4 +1,4 @@
-INSERT INTO `skilllineability_dbc` (`ID`, `SkillLine`, `Spell`, `RaceMask`, `ClassMask`, `MinSkillLineRank`, `SupercededBySpell`, `AcquireMethod`, `TrivialSkillLineRankHigh`, `TrivialSkillLineRankLow`, `CharacterPoints_1`, `CharacterPoints_2`, `TradeSkillCategoryID`) VALUES
+INSERT INTO `skilllineability_dbc` (`ID`, `SkillLine`, `Spell`, `RaceMask`, `ClassMask`, `ExcludeRace`, `ExcludeClass`, `MinSkillLineRank`, `SupercededBySpell`, `AcquireMethod`, `TrivialSkillLineRankHigh`, `TrivialSkillLineRankLow`, `CharacterPoints_1`, `CharacterPoints_2`) VALUES
 (590, 98, 668, 3149, 0, 0, 0, 1, 0, 2, 0, 0, 0),
 (592, 109, 669, 946, 0, 0, 0, 1, 0, 2, 0, 0, 0),
 (3269, 237, 3565, 3149, 128, 0, 0, 1, 0, 0, 0, 0, 0),
