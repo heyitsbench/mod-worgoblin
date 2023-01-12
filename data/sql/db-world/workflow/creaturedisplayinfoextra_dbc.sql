d432a77bdaa40411c20f5bafdab87aa64b623b71
benjymansy123 <benjymansy123@gmail.com>
2023-01-12 13:31:31 -0500

    Add Argent Dawn worgen/goblin displays for DK quests.

---
+++ data/sql/db-world/workflow/creaturedisplayinfoextra_dbc.sql
@@ -0,0 +1,3 @@
+INSERT INTO `creaturedisplayinfoextra_dbc` (`ID`, `DisplayRaceID`, `DisplaySexID`, `SkinID`, `FaceID`, `HairStyleID`, `HairColorID`, `FacialHairID`, `NPCItemDisplay1`, `NPCItemDisplay2`, `NPCItemDisplay3`, `NPCItemDisplay4`, `NPCItemDisplay5`, `NPCItemDisplay6`, `NPCItemDisplay7`, `NPCItemDisplay8`, `NPCItemDisplay9`, `NPCItemDisplay10`, `NPCItemDisplay11`, `Flags`, `BakeName`) VALUES
+(24081, 12, 0, 1, 1, 4, 0, 1, 0, 0, 0, 0, 0, 1883, 0, 0, 0, 13909, 0, 0, 'CreatureDisplayExtra-24081.blp'),
+(24082, 9, 0, 2, 0, 4, 1, 4, 0, 0, 0, 0, 5904, 1883, 0, 0, 0, 13909, 0, 0, 'CreatureDisplayExtra-24082.blp');
