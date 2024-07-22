88999f599991e916d06045fa4715b44797c7dbea
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-07-21 21:12:13 -0400

    Adjust SQL display ID.

--- data/sql/db-world/worgoblin.sql
+++ data/sql/db-world/worgoblin.sql
@@ -120,7 +120,7 @@ DELETE FROM `creature_template_model` WHERE `CreatureID` IN (36613, 46754, 46755
 
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 	(36613, 0, 32385, 1, 1, 12340),
-	(46754, 0, 32549, 1, 1, 12340),
+	(46754, 0, 35249, 1, 1, 12340),
 	(46755, 0, 35250, 1, 1, 12340),
     (55272, 0, 39096, 1, 1, 12340),
 	(55273, 0, 39095, 1, 1, 12340);
